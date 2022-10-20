f72693a01d67f08ecad3af5ed45b337f8fb549dc
Khazim Star <khazim.fikri.9f@gmail.com>
2022-10-20 10:33:44 +0700

    Initialisasi Flutter #2

---
+++ dompetku_app/lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dompetku"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(children: [
+          Text("Ini Home"),
+          Text("Ini Home"),
+        ]),
+      ),
+    );
+  }
+}
